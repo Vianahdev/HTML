dd25c67461dde0c53a620a1a8c50fdc8b6899309
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-06-23 11:55:44 -0300

    propriedade dataset

--- HTML-Avançado/Aula01/script.js
+++ HTML-Avançado/Aula01/script.js
@@ -4,13 +4,30 @@
 //   l_image.setAttribute("src", "./images/deslike.png");
 //});
 
-let l_list = document.querySelector('ul#list');
-let l_num = parseInt(l_list.getAttribute('num'));
 
-let l_contador = "";
+// COLETANDO DADOS ATRAVES DE UM ATRIBUTO NO HTML
+
+
+let l_lista = document.querySelector('ul#lista');
+let l_num = parseInt(l_lista.getAttribute('num'));
+
+let l_conteudo = "";
 
 for(var i = 0; i <= l_num; i++){
-    l_contador += "<li>" + i + "</li>";
+    l_conteudo += "<li>" + i + "</li>";
 }
 
-l_list.innerHTML = l_contador;
+l_lista.innerHTML = l_conteudo;
+
+//Utilizando a propriedade DATASET para coletar os dados de um atributo
+
+let l_list = document.querySelector('ul#list');
+let l_numdata = parseInt(l_list.dataset.num);
+
+let l_conteudodata = "";
+
+for(var i = 1; i <= l_numdata; i++){
+    l_conteudodata += "<li>" + i + "</li>";
+}
+
+l_list.innerHTML = l_conteudodata;
